7f73580ed3bbc54d54a67b1affe02d3c903dd4fe
mrCgg <1697391881@qq.com>
2020-03-17 23:06:02 +0800

    testadd&commit

---
+++ src/main/java/com/demo/utils/test.java
@@ -0,0 +1,13 @@
+package com.demo.utils;
+
+/**
+ * describe:
+ *
+ * @author cgg
+ * @date 2020/03/17
+ */
+public class test {
+    public void test(){
+        System.out.println("test");
+    }
+}
